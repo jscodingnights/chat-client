d39e5ccbf77d4da98c1d49771e3ce4c5248be419
Adam Terlson <adam.terlson@gmail.com>
2016-04-04 20:25:40 +0200

    Removing answer

--- NativeClient/app/modules/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux'
-import chat from './chat'
-import user from './user'
-
-export default combineReducers({ chat, user });
--- NativeClient/app/store/index.js
+++
@@ -1,2 +0,0 @@
-import configureStore from './configureStore';
-module.exports = configureStore();
